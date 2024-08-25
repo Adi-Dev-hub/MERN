96c6436df6c0ff4323c9bad88574a203b6088311
Aditya Dakua <adityadakua21@gmail.com>
2024-08-25 12:15:06 +0530

    deleted everything

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,5 +1,5 @@
 function ListGroup(){
-    return 
+    return (
     <ul className="list-group">
     <li className="list-group-item">An item</li>
     <li className="list-group-item">A second item</li>
@@ -7,5 +7,6 @@ function ListGroup(){
     <li className="list-group-item">A fourth item</li>
     <li className="list-group-item">And a fifth one</li>
   </ul>
+    )
 }
 export default ListGroup
